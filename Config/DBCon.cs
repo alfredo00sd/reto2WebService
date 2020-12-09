b2988ba6d87c96d3cd31ad865b014880e809f9cc
alfredo <alfredoz28mm@gmail.com>
2020-12-08 20:28:15 -0400

    Final touch

--- Config/DBCon.cs
+++ Config/DBCon.cs
@@ -5,7 +5,8 @@ namespace reto2Propietaria
 {
     public class DBCon
     {
-        protected SqlConnection Conexion = new SqlConnection("Server=DESKTOP-EOOHF5T;DataBase=nomina_system;Integrated Security=true");
+        protected SqlConnection Conexion = new SqlConnection("Server=tcp:nomina-server.database.windows.net,1433;Initial Catalog=nomina_system;Persist Security Info=False;User ID=adminNomina;Password=Propietaria2020;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        //protected SqlConnection Conexion = new SqlConnection("Server=DESKTOP-EOOHF5T;DataBase=nomina_system;Integrated Security=true");
 
         public SqlConnection Open()
         {
